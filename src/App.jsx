980198bbd4c8df19cfa38aa360df748485355a26
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-08-30 12:39:24 +0530

    navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-const App=()=>{
-  return(
+import React from "react";
+import Navbar from "./component/Navbar"; // Make sure the path is correct
+import "../src/css/Navbar.css"; // Optional: for global styles
+
+const App = () => {
+  return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
+      <main>
+        {/* <h1>Hello World!</h1> */}
+        {/* Add more conten here */}
+      </main>
     </div>
-  )
-}
+  );
+};
+
 export default App;
