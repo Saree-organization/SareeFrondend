86e516bc6d0cf851f4e47d97c13dcf5e760b3a90
Vishwas-gour <vishwasgour2002@gmail.com>
2025-10-05 15:53:26 +0530

    add pagination to all pages

---
+++ src/data/sareeAddOrFilters.js
@@ -0,0 +1,10 @@
+export const fabricsOptions = ["Silk", "Cotton", "Linen", "Chiffon", "Georgette", "Velvet", "Satin", "Crepe", "Organza"];
+
+export const categoryOptions = ["Wedding", "Party", "Casual", "Festive", "Office", "Traditional", "Bridal", "Cocktail"];
+
+export const colorOptions = ["Red", "Green", "Blue", "Yellow", "Pink", "Purple", "Orange", "Black", "White", "Beige"];
+
+export const minPriceOptions = [500, 1000, 1500, 2000, 2500, 3000];
+
+export const maxPriceOptions = [5000, 10000, 15000, 20000, 25000, 30000];
+
--- src/pages/admin-pages/AdminSarees.jsx
+++ src/pages/admin-pages/AdminSarees.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import API from "../../api/API";
 import "../../css/allSaree.css";
 import SareeCard from "../../components/SareeCard";
+import { fabricsOptions, categoryOptions, colorOptions, minPriceOptions, maxPriceOptions } from "../../data/sareeAddOrFilters";
 
 function AdminSarees() {
   const [filters, setFilters] = useState({
@@ -55,42 +56,41 @@ function AdminSarees() {
   return (
     <div>
       <div className="filter-container">
+
         <select value={filters.fabrics} onChange={(e) => setFilters({ ...filters, fabrics: e.target.value })}>
           <option value="">All Fabrics</option>
-          <option value="Silk">Silk</option>
-          <option value="Cotton">Cotton</option>
-          <option value="Linen">Linen</option>
+          {fabricsOptions.map((f, i) => (
+            <option key={i} value={f}>{f}</option>
+          ))}
         </select>
 
         <select value={filters.category} onChange={(e) => setFilters({ ...filters, category: e.target.value })}>
           <option value="">All Categories</option>
-          <option value="Wedding">Wedding</option>
-          <option value="Party">Party</option>
-          <option value="Casual">Casual</option>
+          {categoryOptions.map((c, i) => (
+            <option key={i} value={c}>{c}</option>
+          ))}
         </select>
 
         <select value={filters.color} onChange={(e) => setFilters({ ...filters, color: e.target.value })}>
           <option value="">All Colors</option>
-          <option value="Red">Red</option>
-          <option value="Green">Green</option>
-          <option value="Blue">Blue</option>
-          <option value="Yellow">Yellow</option>
+          {colorOptions.map((c, i) => (
+            <option key={i} value={c}>{c}</option>
+          ))}
         </select>
 
         <select value={filters.minPrice} onChange={(e) => setFilters({ ...filters, minPrice: e.target.value })}>
           <option value="">Min Price</option>
-          <option value="500">500</option>
-          <option value="1000">1000</option>
-          <option value="2000">2000</option>
+          {minPriceOptions.map((p, i) => (
+            <option key={i} value={p}>{p}</option>
+          ))}
         </select>
 
         <select value={filters.maxPrice} onChange={(e) => setFilters({ ...filters, maxPrice: e.target.value })}>
           <option value="">Max Price</option>
-          <option value="5000">5000</option>
-          <option value="10000">10000</option>
-          <option value="20000">20000</option>
+          {maxPriceOptions.map((p, i) => (
+            <option key={i} value={p}>{p}</option>
+          ))}
         </select>
-
         <button onClick={applyFilters}>Apply Filters</button>
         <button onClick={clearFilters}>Clear</button>
       </div>
