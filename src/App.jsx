ed0d61581263bbc1a44aa3fafc919825681c9b4a
Vishwas-gour <vishwasgour2002@gmail.com>
2025-10-06 18:27:54 +0530

    for admin get all orders not pagiantion

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import {
 
 const App = () => {
   const token = localStorage.getItem("authToken");
-  const role = "admin"; // "user" or "admin"
+  const role = "user"; // "user" or "admin"
 
   return (
     <BrowserRouter>
