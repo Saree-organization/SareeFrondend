94a15e805497eba375b606ff225faf2fd9abc5d6
Vishwas-gour <vishwasgour2002@gmail.com>
2025-10-18 15:22:02 +0530

    style add-saree

--- src/data/sareeAddOrFilters.js
+++ src/data/sareeAddOrFilters.js
@@ -8,6 +8,8 @@ export const designOptions = [
     "Zardosi", "Mirror Work", "Sequins", "Patchwork", "Applique", "Motifs", "Thread Work", "Floral", "Geometric"
 ];
 
+
+
 export const lengthOptions = [5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0]; // more lengths
 
 export const borderOptions = [
